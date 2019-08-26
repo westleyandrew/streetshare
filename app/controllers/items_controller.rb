578c2a7730492734cac53ff9bcad5f00f724f5d5
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-08-26 13:32:17 +0100

    Added controller for Items

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,67 @@
+class ItemsController < ApplicationController
+  # before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @items = Item.all
+
+    # @items = policy_scope(item)
+    # @items = @items.search_by_title_and_model(params[:search]) if params[:search].present?
+    # @items = item.geocoded #returns flats with coordinates
+
+    # @markers = @items.map do |item|
+    #   {
+    #     lat: item.latitude,
+    #     lng: item.longitude,
+    #     infoWindow: render_to_string(partial: "info_window", locals: { item: item }),
+    #     image_url: helpers.asset_url('item.png')
+    #   }
+    # end
+  end
+
+  def show
+    # @review = Review.new
+    # authorize @item
+  end
+
+  def edit
+    # authorize @item
+  end
+
+  def new
+    @item = Item.new
+    # authorize @item
+  end
+
+  def update
+    @item.update(item_params)
+    redirect_to item_path(@item)
+    authorize @item
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+    # authorize @item
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path
+    # authorize @item
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :photo, :address, :longitude, :latitude, :deposit, :categories_id)
+  end
+
+  def set_item
+    @item = item.find(params[:id])
+  end
+end

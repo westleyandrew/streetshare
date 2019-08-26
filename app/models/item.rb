b43b36b88072c97094ed7de046b04cec13ef8ff3
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-08-26 15:35:53 +0100

    Added validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :reviews, dependent: :destroy
+  validates :title, presence: true
+  validates :description, presence: true
 end

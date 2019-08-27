e5f4b701f150f8cbaac6748f44abf0c770eee198
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-08-27 17:21:26 +0100

    Info at map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,8 +14,11 @@ const initMapbox = () => {
 
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
       .addTo(map);
     });
 

de02966a9f3d4d47e758babc7e1f9723c00dba99
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-08-28 13:47:45 +0100

    Algolia fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,8 @@
  // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
-import { initAutocomplete } from '../plugins/init_autocomplete';
 
+import { initAutocomplete } from '../plugins/init_autocomplete';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 initAutocomplete();

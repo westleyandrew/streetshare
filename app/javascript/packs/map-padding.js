afe1ae542ef0f618ef4206c2b81aedaa8bff5f33
dumitru daniel goran <danielgoran7@gmail.com>
2019-09-02 11:22:49 +0100

    map added next side of the cards on index page

---
+++ app/javascript/packs/map-padding.js
@@ -0,0 +1,10 @@
+// var $document = $(document),
+//     $element = $('#map'),
+//     className = 'map-padding';
+
+// $document.scroll(function() {
+//   $element.toggleClass(className, $document.scrollTop() >= 80);
+// });
+
+// let map = document.getElementById("map");
+

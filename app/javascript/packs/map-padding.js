b7ddb8ae0d9790aa72863689cb0cef942e1d56e8
dumitru daniel goran <danielgoran7@gmail.com>
2019-09-02 12:04:06 +0100

    trying to solve small isue of the map on index page with javascript

--- app/javascript/packs/map-padding.js
+++ app/javascript/packs/map-padding.js
@@ -1,3 +1,4 @@
+// import { initMapbox } from '../plugins/init_mapbox'
 // var $document = $(document),
 //     $element = $('#map'),
 //     className = 'map-padding';
@@ -5,6 +6,7 @@
 // $document.scroll(function() {
 //   $element.toggleClass(className, $document.scrollTop() >= 80);
 // });
+// §
 
 // let map = document.getElementById("map");
 

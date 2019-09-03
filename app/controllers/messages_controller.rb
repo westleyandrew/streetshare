adf7d2ebc0ce2b2d268bec9d95cbc21e3e530067
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-09-03 14:08:41 +0100

    Style II

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -27,7 +27,7 @@ class MessagesController < ApplicationController
   def create
     @message = @conversation.messages.new(message_params)
     if @message.save
-      redirect_to conversation_messages_path(@conversation)
+      redirect_to conversations_path
     end
   end
 
